23a83b19d75475c8c054a5541d0df65bf6852201
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-10-27 16:13:33 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -157,7 +157,7 @@ async function getGameList(handlerInput, initIndex, endIndex) {
     const s3Attributes = await attributesManager.getPersistentAttributes() || {};
     const games = s3Attributes.hotnessList;
     console.log(initIndex);
-    console.log(endIndexIndex);
+    console.log(endIndex);
     const gameList = games
                         .map(game => ` <p><emphasis level="strong">${game.rank}</emphasis> <break strength="medium"/> <lang xml:lang="en-US">${game.name}</lang></p>`)
                         .slice(initIndex, endIndex)
