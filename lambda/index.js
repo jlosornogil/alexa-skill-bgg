e0d0fc1b260d69b2b8288caa68668397b0dbe049
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-10-27 15:42:19 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -141,6 +141,7 @@ const ErrorHandler = {
 // The SkillBuilder acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
+/* LAMBDA SETUP */
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
