f9c04c956f1c3f584357b87703490405b27cd6ef
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-10-27 11:39:53 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -23,8 +23,6 @@ const HotnessListIntentHandler = {
     },
     handle(handlerInput) {
         const result = await bgg('hot',{type: 'boardgame'});
-        
-        
         const speakOutput = 'Hello World!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
